6ad5deaabd9e3c7fb300500c295934daf4366918
fredavr <avrillon.frederique@orange.fr>
2025-10-06 16:56:46 +0200

    test presentation

---
+++ asset/js/script.js
@@ -0,0 +1,45 @@
+/*console.log("Hello");
+let age = 25; //number
+let nom = "Fred"; //chaine
+let majeur  = true; //boolean
+const pi = 3.14;
+
+const a = 5;
+let b = 8;
+alert(a += b)
+alert(a == b) //comparaison sur égalité stricte
+alert(a === b) //comparaison sur égalité stricte ET le type de la variable
+alert(a != b) // diffférent de
+alert(a !== b) // diffférent de en tenant compte du type de la variable
+
+
+let anneeNaissance = 1967;
+let anneeEnCours = 2025;
+alert(anneeEnCours - anneeNaissance);
+
+
+let num1 = 5;
+let num2 = 6;
+
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+
+let compteur = 0;
+compteur += 1;
+console.log(compteur);
+
+*/
+
+let age = 12;
+
+if (age >= 18) {
+    console.log("majeur");
+ } else {
+console.log("mineur");
+ }
+
+
+
