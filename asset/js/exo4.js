521bdb1cd1d9a1048d9089eca75ccaf4294f762a
fredavr <avrillon.frederique@orange.fr>
2025-10-13 10:04:50 +0200

    exo 4

---
+++ asset/js/exo4.js
@@ -0,0 +1,15 @@
+let chaineAVerif = prompt("Veuillez saisir la chaine à vérifier");
+
+if (chaineAVerif.toLowerCase() == retournement(chaineAVerif)) {
+    alert("Bravo ! " + chaineAVerif + " est un palyndrome");
+} else {
+    alert("NON " + chaineAVerif + " n'est pas un palyndrome");
+}
+
+function retournement(mot) {
+    let motTab = mot.split("");
+    console.log(motTab);
+    console.log(motTab.reverse());
+    return motTab.join("").toLowerCase();
+}
+
