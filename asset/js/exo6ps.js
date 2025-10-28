eeaf58f34cbcf8dfcd6010a6c4efda2bb2453c4e
fredavr <avrillon.frederique@orange.fr>
2025-10-28 11:14:53 +0100

    exercice 6 série 1

---
+++ asset/js/exo6ps.js
@@ -0,0 +1,19 @@
+function test() {
+    let lePrompt = prompt("La factorielle de quel combre souhaitez-vous calculer ?");
+    let leNombre = parseInt(lePrompt);
+    let laFactorielle = leNombre;
+    console.log("Factorielle de " + leNombre);
+    if (lePrompt == null) {return;}
+
+    if (isNaN(leNombre)) {
+        alert("La saisie est incorrecte : " + lePrompt + "\nVeuillez recommencer le test.");
+        return;
+    }
+
+    for (let i = leNombre - 1; i > 0 ; i--) {
+        console.log(laFactorielle + " * " + i)
+        laFactorielle *= i;
+    }
+    alert(`La factorielle de ${leNombre} est : \n\n ${laFactorielle}`);
+}
+
